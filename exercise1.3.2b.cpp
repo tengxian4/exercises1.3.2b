e22b088b58d0bf456b489ec23b94db41c015b98b
tengxian4 <55143952+tengxian4@users.noreply.github.com>
2019-09-25 09:51:34 +0800

    Add files via upload

---
+++ exercise1.3.2b.cpp
@@ -0,0 +1,16 @@
+/*This is program for box 10x10*/
+#include< iostream>
+using namespace std;
+void main()
+{
+	cout << "10\n9\n8\n";
+	cout << "7           x\n";
+	cout << "6 x\n";
+	cout << "5                    x\n";
+	cout << "4   x\n";
+	cout << "3               x\n";
+	cout << "2     x\n";
+	cout << "1 x\n";
+	cout << "0 1 2 3 4 5 6 7 8 9 10\n\n\n\n";
+	system("pause");
+}
